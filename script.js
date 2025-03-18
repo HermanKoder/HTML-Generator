0f16d3fc2d391f2cc6082f23eb12db2a72c3864d
DezyO <145566704+DezyO@users.noreply.github.com>
2025-03-18 14:11:10 +0100

    .

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-//Endre Fargen på teksten
-var tekstEl = document.getElementById("tekst")
-var fargeEl = document.getElementById("farge")
-var knappEl = document.getElementById("endreFarge")
+// Endre Fargen på teksten
+var tekstEl = document.getElementById("tekst");
+var fargeEl = document.getElementById("farge");
+var knappEl = document.getElementById("endreFargeTekst");
 
-knappEl.addEventListener("click", klikkefunkskjon)
+knappEl.addEventListener("click", function () {
+    if (fargeEl.value) {
+        tekstEl.style.color = fargeEl.value;
+        fargeEl.value = "";
+    } else {
+        alert("Skriv inn en gyldig farge på engelsk.");
+    }
+});
 
-function klikkefunkskjon(e){
-    tekstEl.style.color = fargeEl.value;
-    fargeEl.value = ""
-}
+// Endre bakgrunnsfarge
+var radioButtons = document.querySelectorAll("input[name='fargeBG']");
+
+radioButtons.forEach(function (radio) {
+    radio.addEventListener("change", function () {
+        document.body.style.backgroundColor = radio.value;
+    });
+});
